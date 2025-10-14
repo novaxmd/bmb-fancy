239ddff8174320a75a9f54cf6b734fe1cb16a932
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-10-14 12:28:36 +0300

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0095f6',
+      },
+      animation: {
+        'ping-once': 'ping 600ms cubic-bezier(0, 0, 0.2, 1)',
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+
+  daisyui: {
+    themes: ['light'],
+  },
+}
